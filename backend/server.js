573a115cff11fdd8f6e2c10b03858f1b13283b22
Akib Mulla <akibmulla47@gmail.com>
2025-03-04 01:53:07 +0530

    added backend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
